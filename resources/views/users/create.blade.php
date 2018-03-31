dc3d6cdd0eeebec4ebd80433bd8b53923e522637
Yang di <yangdi@hotmail.com>
2018-03-31 19:30:51 +0800

    Finish user signup

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.default')
 @section('title','注册')
 @section('content')
-  <h1>注册</h1>
+ <div class="offset-md-2 col-md-8">
+  <div class="card">
+    <div class="card-header">
+      <h5>Register</h5>
+    </div>
+    <div class="card-body">
+    	@include('shared._errors')
+
+      <form method="POST" action="{{ route('users.store') }}">
+      	{{ csrf_field() }}
+
+          <div class="form-group">
+            <label for="name">User ID:</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">Confirm Password:</label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Register</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
